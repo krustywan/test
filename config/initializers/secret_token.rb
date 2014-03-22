4ea78763316727be4c9e1d63612a3abb459b84d9
krustywan <kristy@kristyhaines.com>
2014-03-22 16:30:58 +0800

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SquigglebumCharacter::Application.config.secret_key_base = '101fc5992aeb118c50b84a1e340466fe0406557a7851cad69346dff0b790a05516ddbe293f130954908935b9653652efc897847b06445df0908990b8f02c64b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SquigglebumCharacter::Application.config.session_store :cookie_store, key: '_squigglebum-character_session'
